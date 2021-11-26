a2153a30ac5bd0142dfe086b12312a777c2be7c5
Anna Gawrysiak <70595292+AnnaGawrysiak@users.noreply.github.com>
2021-11-26 10:41:49 +0100

    Modify the Game class.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import random
 
 SIGN_CROSS = 'X'
@@ -8,7 +7,7 @@ SIGN_EMPTY = '_'
 
 class Player:
 
-    def get_position(self):
+    def get_position(self, b1):
         pass
 
 
@@ -17,38 +16,57 @@ class HumanPlayer(Player):
         self._name = who
         self._sign = cross_or_circle_sign
 
-    # def name(self):
-    #     return self._name
-    #
-    # def sign(self):
-    #     return self._sign
-
     @property
     def name(self):
-        # """ The name property"""
         return self._name
-
-    # @name.setter
-    # def name(self, value):
-    #     self._name = value
 
     @property
     def sign(self):
-        # """ The sign property"""
         return self._sign
 
-    def get_position(self):  # od 0..8
-        pass  # TODO:
-
-    # @sign.setter
-    # def sign(self, value):
-    #     self._sign = value
+    def get_position(self, b1):
+        print(f"Player {self._name} : tell me which field on a board should be marked .")
+        field_nr = input("Enter field number (1 to 9): ")
+        position = int(field_nr) - 1
+        return position
 
 
 class ComputerPlayer(Player):
 
-    def get_position(self):  # od 0..8
-        pass
+    def __init__(self):
+        self._name = "Computer "
+        self._sign = SIGN_CIRCLE
+
+    def get_position(self, b1):
+        possible_moves = [key for (key, value) in b1 if value != SIGN_EMPTY]
+
+        for i in possible_moves:
+            b1[i] = SIGN_CIRCLE
+            if b1.check_for_win(SIGN_CIRCLE):
+                return b1.insert(i, SIGN_CIRCLE)
+            b1[i] = SIGN_CROSS
+            if b1.check_for_win(SIGN_CROSS):
+                return i
+
+        corners_open = []
+
+        for j in possible_moves:
+            if j in [1, 3, 7, 9]:
+                corners_open.append(j)
+
+        if len(corners_open) > 0:
+            return random.choice(corners_open)
+
+        if 5 in possible_moves:
+            return 5
+
+        edges_open = []
+        for i in possible_moves:
+            if i in [2, 4, 6, 8]:
+                edges_open.append(i)
+
+        if len(edges_open) > 0:
+            return random
 
 
 class OccupiedFieldError(Exception):
@@ -71,20 +89,7 @@ class Board:
 
     @property
     def board(self):
-        # """ The board property"""
         return self._board
-
-    # @board.setter
-    # def board(self, value):
-    #     self._board = value
-
-    # def print(self):
-    #     print(self._board[0] + '|' + self._board[1] + "|" + self._board[2])
-    #     print("-" + "+" + "-" + "+" + "-")
-    #     print(self._board[3] + '|' + self._board[4] + "|" + self._board[5])
-    #     print("-" + "+" + "-" + "+" + "-")
-    #     print(self._board[6] + '|' + self._board[7] + "|" + self._board[8])
-    #     print('\n')
 
     def __str__(self):
         return '[%s | % s| %s]\n[%s | % s| %s]\n[%s | % s| %s]' % (
@@ -125,23 +130,9 @@ class Board:
             raise PositionError()
         elif self._board[position] == SIGN_EMPTY:
             self._board[position] = sign
-            # if self.check_for_win(sign):
-            #     print("Congratulations! You won!")
-            #     sys.exit()
-            # if self.is_full():
-            #     print("The board is full. Game over.")
-            #     sys.exit()
         else:
             raise OccupiedFieldError()
 
-
-# b = Board()
-# b.insert(0, SIGN_CROSS)
-# b.insert(1, SIGN_CROSS)
-# b.insert(2, SIGN_CROSS)
-# assert b.check_for_win(SIGN_CROSS) == True
-
-# SOLID
 
 class Game:
     def __init__(self, board, player1, player2):
@@ -152,7 +143,7 @@ class Game:
     def run(self):
         player = self._player1
         while True:
-            pos = player.get_position()  # *HumanPlayer* gdy prosimy o pozycję
+            pos = player.get_position(self._board)
             self._board.insert(pos, player.sign)
             if self._board.check_for_win(player.sign):
                 return player
@@ -164,108 +155,29 @@ class Game:
                 player = self._player1
 
 
-# def xsum(numbers):
-#     s = 0
-#     for number in numbers:
-#         s += number
-#     return s
-
-
 def get_players():
-    while True:
-        choice = input("Select game mode. Press '1' for single player mode and '2' for two players mode.")
-        if choice not in ('1', '2'):
+
+    choice = input("Select game mode. Press '1' for single player mode and '2' for two players mode.")
+
+    while choice not in ('1', '2'):
             print("Not an appropriate choice.")
-        else:
-            break
+            choice = input("Select game mode. Press '1' for single player mode and '2' for two players mode.")
+
+    #import pdb; pdb.set_trace()
 
     if choice == '1':
-
-        return HumanPlayer(), ComputerPlayer()
+        name1 = input("Tell me your name: ")
+        return HumanPlayer(name1, SIGN_CROSS), ComputerPlayer()
     else:
-        return HumanPlayer(sign=), HumanPlayer()
+        name1 = input("Player 1, tell me your name: ")
+        name2 = input("Player 2, tell me your name: ")
+        return HumanPlayer(name1, SIGN_CROSS), HumanPlayer(name2, SIGN_CIRCLE)
 
 
-player1, player2 = get_players()
-g = Game(player1=None, player2=None)
-g.run()
+def main():
+    player1, player2 = get_players()
+    game = Game(Board.board, player1, player2)
+    game.run()
 
 
-class Game:
-
-    def __init__(self):
-        print("Game started")
-        pass
-
-    def _make_a_move(self, current_player, b1):
-        print(f"Player {current_player.name} : tell me which field on a board should be marked .")
-        field_nr = input("Enter field number (1 to 9): ")
-        position = int(field_nr) - 1
-        b1.insert(position, current_player.sign)
-        b1.print()
-
-    def _AI_move(self, b1):  # AI sign is Circle
-        possible_moves = [key for (key, value) in b1 if value != SIGN_EMPTY]
-
-        for i in possible_moves:
-            b1[i] = SIGN_CIRCLE
-            if b1.check_for_win(SIGN_CIRCLE):
-                return b1.insert(i, SIGN_CIRCLE)
-            b1[i] = SIGN_CROSS
-            if b1.check_for_win(SIGN_CROSS):
-                return b1.insert(i, SIGN_CIRCLE)
-
-        corners_open = []
-
-        for j in possible_moves:
-            if j in [1, 3, 7, 9]:
-                corners_open.append(j)
-
-        if len(corners_open) > 0:
-            return b1.insert(random.choice(corners_open), SIGN_CIRCLE)
-
-        if 5 in possible_moves:
-            return b1.insert(5, SIGN_CIRCLE)
-
-        edges_open = []
-        for i in possible_moves:
-            if i in [2, 4, 6, 8]:
-                edges_open.append(i)
-
-        if len(edges_open) > 0:
-            return b1.insert(random.choice(edges_open), SIGN_CIRCLE)
-
-    def run(self):
-        b1 = Board()
-        print("Welcome to the Tic Tac Toe Game.")
-
-        while True:
-            choice = input("Select game mode. Press '1' for single player mode and '2' for two players mode.")
-            if choice not in ('1', '2'):
-                print("Not an appropriate choice.")
-            else:
-                break
-
-        name1 = input("Player 1. Please enter your name: ")
-        player1 = Player(name1, SIGN_CROSS)
-        print(f"Player {name1}: your sign is a cross.\n")
-
-        if choice == '2':
-            name2 = input("Player 2. Please enter your name: ")
-            player2 = Player(name2, SIGN_CIRCLE)
-            print(f"Player {name2}: your sign is a circle.\n")
-            while True:
-                self._make_a_move(player1, b1)
-                self._make_a_move(player2, b1)
-        elif choice == '1':
-            while True:
-                self._make_a_move(player1, b1)
-                b1.print()
-                self._AI_move(b1)
-                b1.print()
-
-# def main():
-#     game = Game()
-#     game.run()
-
-# main()
+main()
